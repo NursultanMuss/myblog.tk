6f0b17179b4f6d04a7292aaa5a9ce79d4707e64a
Nursultan HomePC <nursalga@gmail.com>
2016-11-04 01:54:11 +0600

    04.11.2016 Home

---
+++ views/site/blog_cat.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HomeO
+ * Date: 04.11.2016
+ * Time: 1:49
+ */
---
+++ views/site/prog_cat.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HomeO
+ * Date: 03.11.2016
+ * Time: 20:32
+ */
